0eeed2ed94cd6b5fd8d42522f5b2c3d23c64e9c4
Chi Nguyen Dac <dacchi6761@gmail.com>
2022-10-16 12:04:29 +0700

    update config

--- craco.config.js
+++ craco.config.js
@@ -8,12 +8,12 @@ module.exports = {
             env === 'development' && require.resolve('react-dev-utils/webpackHotDevClient'),
             paths.appIndexJs,
           ].filter(Boolean),
-          background: './src/chromeServices/background.ts',
-          contentScript: './src/chromeServices/contentScript.ts',
+          background: './src/chromeServices/backgroundScript.ts',
+          content: './src/chromeServices/contentScript.ts',
         },
         output: {
           ...webpackConfig.output,
-          filename: '[name].js',
+          filename: 'static/js/[name].js',
         },
         optimization: {
           ...webpackConfig.optimization,
