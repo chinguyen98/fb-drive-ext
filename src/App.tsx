edcf871f534216222fb5bb1473869424557cb3b6
Chi Nguyen Dac <dacchi6761@gmail.com>
2022-10-17 21:45:17 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from './_app.module.scss';
 
 function App() {
-  return <div className={styles['container']}>Hello</div>;
+  return <div className={styles['container']}>Hello 1</div>;
 }
 
 export default App;
--- src/chromeServices/backgroundScript.ts
+++ src/chromeServices/backgroundScript.ts
@@ -1,3 +1,17 @@
-const test = () => {};
+let enabledRule: chrome.events.Rule = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { hostSuffix: '.facebook.com', schemes: ['https'] },
+    }),
+  ],
+  actions: [new chrome.declarativeContent.ShowAction()],
+};
 
-export default test;
+chrome.runtime.onInstalled.addListener((details) => {
+  chrome.action.disable();
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([enabledRule]);
+  });
+});
+
+export {};
