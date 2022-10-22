d3bdb91b6dae6b3582fefe5383e691c794fae4f6
Chi Nguyen Dac <dacchi6761@gmail.com>
2022-10-22 22:21:27 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { CLIENT_ID } from './config';
 import styles from './_app.module.scss';
 
 function App() {
-  return <div className={styles['container']}>Hello 1</div>;
+  return <div className={styles['container']}></div>;
 }
 
 export default App;
---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
